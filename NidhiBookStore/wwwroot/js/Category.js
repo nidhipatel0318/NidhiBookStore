cf3226a0fb7c5b60fafd6d7d14ba7c60b00a345c
nidhipatel0318 <135552374+nidhipatel0318@users.noreply.github.com>
2023-11-20 23:18:32 -0500

    again the typos deleting the category and now moving on part 3

--- NidhiBookStore/wwwroot/js/Category.js
+++ NidhiBookStore/wwwroot/js/Category.js
@@ -20,7 +20,7 @@ function loadDataTable() {
                                 <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
                                     <i class="fas fa-edit"></i>&nbsp;
                                 </a>
-                                <a onclick=Delete("/Admin/Category/Delete/${data}")class="btn btn-danger text-white" style="cursor:pointer">
+                                <a onclick=Delete("/Admin/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
                                     <i class="fas fa-trash-alt"></i>&nbsp;
                                 </a>
                             </div>
@@ -30,10 +30,11 @@ function loadDataTable() {
         ]
     });
 }
+
 function Delete(url) {
     swal({
         title: "Are you sure you want to delete?",
-        text: "You will not be able to restore the data!",
+        text: "you will not be able to restore the data!",
         icon: "warning",
         buttons: true,
         dangerMode: true
@@ -53,5 +54,5 @@ function Delete(url) {
                 }
             });
         }
-    });
+    })
 }
